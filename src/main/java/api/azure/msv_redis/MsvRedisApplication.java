d60bd76b146921b4b52d779d55190d9d413dcfa3
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-11-23 17:38:04 -0500

    first commit

---
+++ src/main/java/api/azure/msv_redis/MsvRedisApplication.java
@@ -0,0 +1,13 @@
+package api.azure.msv_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvRedisApplication.class, args);
+	}
+
+}
